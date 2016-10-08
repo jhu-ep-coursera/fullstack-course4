2c7417238f47c6588d46d405bff1cca393e532f2
freeranger <tony.butler@me.com>
2016-10-08 09:29:53 +0100

    Issue 30 - window replaced with global

--- examples/Lecture58/js/ajax-utils.js
+++ examples/Lecture58/js/ajax-utils.js
@@ -6,10 +6,10 @@ var ajaxUtils = {};
 
 // Returns an HTTP request object
 function getRequestObject() {
-  if (window.XMLHttpRequest) {
+  if (global.XMLHttpRequest) {
     return (new XMLHttpRequest());
   } 
-  else if (window.ActiveXObject) {
+  else if (global.ActiveXObject) {
     // For very old IE browsers (optional)
     return (new ActiveXObject("Microsoft.XMLHTTP"));
   } 
