4958ff2171d7ad60090f45ad5aa7621d11bbb252
Nisha1607 <57458313+Nisha1607@users.noreply.github.com>
2020-05-17 01:00:07 +0530

    Update SpeakGoodBye.js

--- assignments/assignment4/assignment4-solution-starter/easier/SpeakGoodBye.js
+++ assignments/assignment4/assignment4-solution-starter/easier/SpeakGoodBye.js
@@ -11,15 +11,22 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+(function(){
+	var byeSpeaker = {};
+
+// DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 var speakWord = "Good Bye";
+byeSpeaker.speak = function (name) {
+  		console.log(speakWord + " " + name);
+	}
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
 
+window.byeSpeaker = byeSpeaker;
+
+})();
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
