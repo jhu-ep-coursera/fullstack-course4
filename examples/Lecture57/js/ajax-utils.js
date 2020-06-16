aa58273e5084f975e4fd09b1f62df1c0a6b99708
YASH SANJAY SALVI <46518495+Raxy45@users.noreply.github.com>
2020-06-16 13:06:52 +0530

    Done

--- examples/Lecture57/js/ajax-utils.js
+++ examples/Lecture57/js/ajax-utils.js
@@ -9,6 +9,8 @@ function getRequestObject() {
   if (global.XMLHttpRequest) {
     return (new XMLHttpRequest());
   } 
+ //5 RETURNS WITH XMMLHTTPREQUEST AND ASSIGNS TO VARIABLE "REQUEST" BELOW
+ 
   else if (global.ActiveXObject) {
     // For very old IE browsers (optional)
     return (new ActiveXObject("Microsoft.XMLHTTP"));
@@ -21,13 +23,18 @@ function getRequestObject() {
 
 
 // Makes an Ajax GET request to 'requestUrl'
+//3 "REQUESTURL" CONTAINS DATA.TXT PATH AND "RESPONSE HANDLER" contans F/N IN SCRIPT(refer names below)
 ajaxUtils.sendGetRequest = 
   function(requestUrl, responseHandler) {
     var request = getRequestObject();
+//4 JUMPS ABOVE
+ //7 .onreadystatechange changes state when server returns with some response(See ww3School for more)
     request.onreadystatechange = 
       function() { 
         handleResponse(request, responseHandler); 
-      };
+  //8 handle response called and goes below
+    };
+ //6 SENDS REQUEST TO REQURL PATH i.e. data.txt
     request.open("GET", requestUrl, true);
     request.send(null); // for POST only
   };
@@ -36,11 +43,17 @@ ajaxUtils.sendGetRequest =
 // Only calls user provided 'responseHandler'
 // function if response is ready
 // and not an error
+
+
 function handleResponse(request,
                         responseHandler) {
   if ((request.readyState == 4) &&
      (request.status == 200)) {
+       console.log(request.readyState);
+       console.log(request.status);
+       
     responseHandler(request);
+ //9 gets the request which contains response and the status like 200 etc checked and if true F/N in script called
   }
 }
 
--- examples/Lecture57/js/script.js
+++ examples/Lecture57/js/script.js
@@ -1,4 +1,5 @@
 // Event handling
+//1
 document.addEventListener("DOMContentLoaded",
   function (event) {
     
@@ -7,15 +8,17 @@ document.addEventListener("DOMContentLoaded",
       .addEventListener("click", function () {
         
         // Call server to get the name
+     //2 PASSED TO AJAX IN AJAX-UTILS along with URL and F/N
         $ajaxUtils
           .sendGetRequest("data/name.txt", 
+     //10 .responsetext is special property which contains text that comes along with the response
             function (request) {
               var name = request.responseText;
 
               document.querySelector("#content")
                 .innerHTML = "<h2>Hello " + name + "!</h2>";
             });
-
+      //11 here docx.query .... on line number 18 needs to be inside f/n to go against asynchronous property
         
       });
   }
