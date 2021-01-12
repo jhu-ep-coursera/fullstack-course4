9d3921ef04321fcf3ffc49869278b33162b4e761
Yaakov Chaikin <ychaikin@users.noreply.github.com>
2021-01-12 12:45:16 -0500

    Updated language of the step
    Thanks @carloscaeduardo for point out the typo.

--- assignments/assignment4/assignment4-solution-starter/harder/script.js
+++ assignments/assignment4/assignment4-solution-starter/harder/script.js
@@ -38,8 +38,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
-// using the 'speak' method or either helloSpeaker's or byeSpeaker's
-// 'speak' method.
+// using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
 for (/* fill in parts of the 'for' loop to loop over names array */) {
 
