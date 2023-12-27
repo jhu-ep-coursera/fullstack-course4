6a59f4b7031d4d4824eb36203c3dc671005ad5fa
git <147007244+Test-my-future@users.noreply.github.com>
2023-12-27 06:14:02 +0200

    Delete examples directory

--- examples/Lecture44/js/script.js
+++
@@ -1,8 +0,0 @@
-// Default values
-function orderChickenWith(sideDish) {
-  sideDish = sideDish || "whatever!";
-  console.log("Chicken with " + sideDish);
-}
-
-orderChickenWith("noodles");
-orderChickenWith();
